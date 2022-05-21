31f2d2484c4f8329fed22b8fb70aaf5145bc46d0
aismam <ismamabu25@gmail.com>
2022-05-21 16:37:10 +0200

    Added base tests

---
+++ app/src/test/scala/Tower/TowerTest.scala
@@ -0,0 +1,16 @@
+package Tower
+
+import org.junit.runner.RunWith
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.junit.JUnitRunner
+
+class TowerTest {
+
+  @RunWith(classOf[JUnitRunner])
+  class AppSuite extends AnyFunSuite {
+
+    test("Tower test example") {
+      assert(true)
+    }
+  }
+}
