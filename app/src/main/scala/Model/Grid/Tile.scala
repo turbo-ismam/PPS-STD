e038930ec56eda6b1623217d9d196100b08fc03f
aismam <ismamabu25@gmail.com>
2022-05-26 23:26:07 +0200

    Added some basic entities

--- app/src/main/scala/Model/Grid/Tile.scala
+++ app/src/main/scala/Model/Grid/Tile.scala
@@ -1,5 +1,15 @@
 package Model.Grid
 
-trait Tile {
+import Model.Grid.Tiles.TileType
+
+class Tile(val x: Int, val y: Int, val tType: TileType){
+
+  def tileType: TileType = tType
+
+  def draw(): Unit = ???
+
+  def xPlace: Int = x / 64
+
+  def yPlace: Int = y / 64
 
 }
---
+++ app/src/main/scala/Model/Grid/Tiles/TileTypes.scala
@@ -0,0 +1,8 @@
+package Model.Grid.Tiles
+
+object TileTypes extends Enumeration {
+
+  type TileType = Value
+
+  val Path, Grass, Nothing = Value
+}
