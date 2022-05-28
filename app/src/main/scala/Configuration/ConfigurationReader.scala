f6a3566e0f2a0f7435755c1e603b83d4655dcdcc
aismam <ismamabu25@gmail.com>
2022-05-28 16:38:08 +0200

    Fix, refactor and test on Properties

--- app/src/main/scala/Configuration/ConfigurationReader.scala
+++ app/src/main/scala/Configuration/ConfigurationReader.scala
@@ -1,6 +1,6 @@
 package Configuration
 
-import java.io.{FileNotFoundException}
+import java.io.FileNotFoundException
 import java.net.URL
 import java.util.Properties
 import scala.io.Source
@@ -9,25 +9,25 @@ class ConfigurationReader() {
 
   val properties: Properties = new Properties()
 
-  // users set this property specifying the file application.properties (if custom) otherwise we use default
+  // users set this property specifying the file application.properties (if custom) otherwise use the default one
   var config: String = System.getProperty("configuration.path")
-  var configurationPath: URL = this.getClass.getClassLoader.getResource("application.properties")
+  var configurationPath: Option[URL] = None
 
-  if (config != null) {
-    configurationPath = new URL(config)
-  }
-
+  configurationPath = if (config == null)
+    Some(this.getClass.getClassLoader.getResource("application.properties")) else Some(new URL(config))
 
   try {
-    val source = Source.fromURL(configurationPath)
-    properties.load(source.bufferedReader())
-    properties.forEach((k,v) => System.setProperty(k.toString,v.toString))
+    configurationPath match {
+      case Some(cp) =>
+        val source = Source.fromURL(cp)
+        properties.load(source.bufferedReader())
+        properties.forEach((k, v) => System.setProperty(k.toString, v.toString))
+    }
   } catch {
-    case e: FileNotFoundException =>
+    case _: FileNotFoundException =>
       println("Configuration file " + configurationPath + " does not exist")
       System.exit(1)
   }
-
 
 
 }
