2ed22a44c1f8b3284e746a0f67027555015f7a50
VladMattiussi <vlad.mattiussi@studio.unibo.it>
2022-05-30 00:19:41 +0200

    enemy positioning

--- app/src/main/scala/Model/Enemy/EnemyImpl.scala
+++ app/src/main/scala/Model/Enemy/EnemyImpl.scala
@@ -1,15 +1,23 @@
 package Model.Enemy
 
+import Controller.DrawingManager
 import Model.Grid.{Grid, Tile}
 
-class EnemyImpl(enemytype: EnemyType) extends Enemy {
+class EnemyImpl(enemytype: EnemyType, grid: Grid) extends Enemy {
 
-    //private val startTile = grid
+    //val path = grid.getGrid.iterator.indexWhere(n => n = 1)
+    val startTile = grid.getGrid(1)(0)
+    var actualTile : Tile = startTile
+    var x = startTile.x
+    var y = startTile.y
     var health: Int = enemytype.health
     val speed: Int = enemytype.speed
     var alive: Boolean = false
 
-  override def draw(): Unit = null
+  override def draw(): Unit = {
+    DrawingManager.drawTile(x.toDouble, y.toDouble, Easy.color)
+    DrawingManager.print()
+  }
 
   override def spawn(): Unit = {
     this.alive = true
@@ -17,7 +25,9 @@ class EnemyImpl(enemytype: EnemyType) extends Enemy {
 
   override def move(t: Tile): Unit = ???
 
-  override def currentTile(): Tile = ???
+  override def currentTile(): Tile = {
+    grid.getGrid(actualTile.xPlace)(actualTile.yPlace)
+  }
 
   override def takeDamage( i: Int): Unit = {
     this.health -= i
