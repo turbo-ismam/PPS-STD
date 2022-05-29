792347300102b61eb7acffe74ea0677ae38c2f54
Hamado Dene <hamadodene@yahoo.com>
2022-05-29 10:52:33 +0200

    Revert "Added some basic entities"

--- app/src/main/scala/Model/Grid/Tile.scala
+++ app/src/main/scala/Model/Grid/Tile.scala
@@ -1,19 +1,5 @@
 package Model.Grid
 
-import Controller.DrawingManager
-import Model.Grid.Tiles.TileType
-
-class Tile(val x: Int, val y: Int, val tType: TileType){
-
-  def tileType: TileType = tType
-
-  def draw(): Unit = {
-    DrawingManager.drawTile(x, y, tileType.col)
-    DrawingManager.print()
-  }
-
-  def xPlace: Int = x / 64
-
-  def yPlace: Int = y / 64
+trait Tile {
 
 }
--- app/src/main/scala/Model/Grid/Tiles/TileTypes.scala
+++
@@ -1,8 +0,0 @@
-package Model.Grid.Tiles
-
-object TileTypes extends Enumeration {
-
-  type TileType = Value
-
-  val Path, Grass, Nothing = Value
-}
