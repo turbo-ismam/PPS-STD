204dbdd3b50b6983cee28509e1edd495664a98fd
VladMattiussi <vlad.mattiussi@studio.unibo.it>
2022-06-23 18:08:32 +0200

    enemy refactor

--- app/src/main/scala/Model/Enemy/EnemyImpl.scala
+++ app/src/main/scala/Model/Enemy/EnemyImpl.scala
@@ -75,110 +75,72 @@ class EnemyImpl(enemyType: EnemyType, gridController: GridController) extends En
   override def move(delta:Double): Unit = {
     //All the surrounding tiles.
     val t = this.actualTile
-    var u = gridController.gameGrid(0)(0)
-    var l = gridController.gameGrid(0)(0)
-    var d = gridController.gameGrid(0)(0)
-    var r = gridController.gameGrid(0)(0)
+    var up = gridController.gameGrid(0)(0)
+    var down = gridController.gameGrid(0)(0)
+    var right = gridController.gameGrid(0)(0)
+    var left = gridController.gameGrid(0)(0)
     if (t.yPlace != 0) {
-      u = gridController.gameGrid(t.yPlace - 1)(t.xPlace)
+      up = gridController.gameGrid(t.yPlace - 1)(t.xPlace)
     }
     else {
-      u = gridController.gameGrid(t.yPlace)(t.xPlace)
+      up = gridController.gameGrid(t.yPlace)(t.xPlace)
     }
 
     if (t.yPlace != 19) {
-      d = gridController.gameGrid(t.yPlace + 1)(t.xPlace)
+      down = gridController.gameGrid(t.yPlace + 1)(t.xPlace)
     }
     else {
-      d = gridController.gameGrid(t.yPlace)(t.xPlace)
+      down = gridController.gameGrid(t.yPlace)(t.xPlace)
     }
 
     if (t.xPlace != 19) {
-      r = gridController.gameGrid(t.yPlace)(t.xPlace + 1)
+      right = gridController.gameGrid(t.yPlace)(t.xPlace + 1)
     }
     else {
-      r = gridController.gameGrid(t.yPlace)(t.xPlace)
+      right = gridController.gameGrid(t.yPlace)(t.xPlace)
     }
 
     if (t.xPlace != 0) {
-      l = gridController.gameGrid(t.yPlace)(t.xPlace - 1)
+      left = gridController.gameGrid(t.yPlace)(t.xPlace - 1)
     }
     else {
-      l = gridController.gameGrid(t.yPlace)(t.xPlace)
+      left = gridController.gameGrid(t.yPlace)(t.xPlace)
     }
 
 
-    //Enemy cant turn 180 degrees around so current value of dirMulti cant be opposite.
-    if ((u.tType.tileType == TileTypes.EndTile || u.tType.tileType == TileTypes.Path || u.tType.tileType == t.tType.tileType) && dirMulti != (0, 1) && (dir_val_check == 0 || dir_val_check == 1)) {
-      if(!this.dir_check){
-        dirMulti = (0, -1)
-        this.dir_check = true
-      }
-      if (x > u.x - 10 && x < u.x + 10 && y > u.y - 10 && y < u.y + 10) {
-        x = u.x
-        y = u.y
-        this.actualTile = gridController.gameGrid(u.yPlace)(u.xPlace)
-        this.dir_check = false
-        this.dir_val_check = 0
-      }
-      else{
-        x +=  delta*speed*dirMulti._1
-        y +=  delta*speed*dirMulti._2
-        this.dir_val_check = 1
-      }
+    dir_val_check match {
+      case 0 | 1 if (up.tType.tileType == TileTypes.EndTile || up.tType.tileType == TileTypes.Path || up.tType.tileType == t.tType.tileType) && dirMulti != (0, 1) => moveToPosition(up,"up",delta)
+      case 0 | 2 if (down.tType.tileType == TileTypes.EndTile || down.tType.tileType == TileTypes.Path || down.tType.tileType == t.tType.tileType) && dirMulti != (0, -1) => moveToPosition(down,"down",delta)
+      case 0 | 3 if (right.tType.tileType == TileTypes.EndTile || right.tType.tileType == TileTypes.Path || right.tType.tileType == t.tType.tileType) && dirMulti != (-1, 0) => moveToPosition(right,"right",delta)
+      case 0 | 4 if (left.tType.tileType == TileTypes.EndTile || left.tType.tileType == TileTypes.Path || left.tType.tileType == t.tType.tileType) && dirMulti != (1, 0) => moveToPosition(left,"left",delta)
+    }
+  }
 
-    } else if ((d.tType.tileType == TileTypes.EndTile || d.tType.tileType == TileTypes.Path || d.tType.tileType == t.tType.tileType) && dirMulti != (0, -1) && (dir_val_check == 0 || dir_val_check == 2)) {
-      if(!this.dir_check){
-        dirMulti = (0, 1)
-        this.dir_check = true
+  def moveToPosition(tile: Tile, direction: String, delta: Double): Unit = {
+    if(!this.dir_check){
+      direction match {
+        case "up" => dirMulti = (0, -1)
+        case "down" => dirMulti = (0, 1)
+        case "right" => dirMulti = (1, 0)
+        case "left" => dirMulti = (-1, 0)
       }
-      if (x > d.x - 10 && x < d.x + 10 && y > d.y - 10 && y < d.y + 10) {
-        x = d.x
-        y = d.y
-        this.actualTile = gridController.gameGrid(d.yPlace)(d.xPlace)
-        this.dir_check = false
-        this.dir_val_check = 0
-      }
-      else{
-        x +=  delta*speed*dirMulti._1
-        y +=  delta*speed*dirMulti._2
-        this.dir_val_check = 2
-      }
-
-    } else if ((r.tType.tileType == TileTypes.EndTile || r.tType.tileType == TileTypes.Path || r.tType.tileType == t.tType.tileType) && dirMulti != (-1, 0) && (dir_val_check == 0 || dir_val_check == 3)) {
-      if(!this.dir_check){
-        dirMulti = (1, 0)
-        this.dir_check = true
-      }
-      if (x > r.x - 10 && x < r.x + 10 && y > r.y - 10 && y < r.y + 10) {
-        x = r.x
-        y = r.y
-        this.actualTile = gridController.gameGrid(r.yPlace)(r.xPlace)
-        this.dir_check = false
-        this.dir_val_check = 0
-      }
-      else{
-        x +=  delta*speed*dirMulti._1
-        y +=  delta*speed*dirMulti._2
-        this.dir_val_check = 3
-      }
-
-    } else if ((l.tType.tileType == TileTypes.EndTile || l.tType.tileType == TileTypes.Path || l.tType.tileType == t.tType.tileType) && dirMulti != (1, 0) && (dir_val_check == 0 || dir_val_check == 4)) {
-      if(!this.dir_check){
-        dirMulti = (-1, 0)
-        this.dir_check = true
-      }
-      if (x > l.x - 10 && x < l.x + 10 && y > l.y - 10 && y < l.y + 10) {
-        x = l.x
-        y = l.y
-        this.actualTile = gridController.gameGrid(l.yPlace)(l.xPlace)
-        this.dir_check = false
-        this.dir_val_check = 0
-      }
-      else{
-        x +=  delta*speed*dirMulti._1
-        y +=  delta*speed*dirMulti._2
-        this.dir_val_check = 4
+      this.dir_check = true
+    }
+    if (x > tile.x - 10 && x < tile.x + 10 && y > tile.y - 10 && y < tile.y + 10) {
+      x = tile.x
+      y = tile.y
+      this.actualTile = gridController.gameGrid(tile.yPlace)(tile.xPlace)
+      this.dir_check = false
+      this.dir_val_check = 0
+    }
+    else{
+      x +=  delta*speed*dirMulti._1
+      y +=  delta*speed*dirMulti._2
+      direction match {
+        case "up" => this.dir_val_check = 1
+        case "down" => this.dir_val_check = 2
+        case "right" => this.dir_val_check = 3
+        case "left" => this.dir_val_check = 4
       }
     }
   }
